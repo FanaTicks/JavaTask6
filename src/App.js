cdfe49aad51ff729a4f8812da2ba12afe6e1bdc6
Alexander Pukhalskyi <p.vadim974@gmail.com>
2023-01-31 09:53:16 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
     } = this.state
 
     const listItems = history.map((number) =>
-        <li>{number}</li>
+        <li  key={number}>{number}</li>
     );
 
     const clear = () => {
